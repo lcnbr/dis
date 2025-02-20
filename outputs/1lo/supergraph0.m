2be99f44d0cae8ddf1510469d3b3b61f8a305b5e
Lucien Huber <im@lcnbr.ch>
2025-02-20 15:29:22 +0000

    generate all new outputs

--- outputs/1lo/supergraph0.m
+++ outputs/1lo/supergraph0.m
@@ -1,34 +1,34 @@
 {
   <|
-    "Denominator" -> prop[0,q]^-2,
-    "Partial_fraction" -> {
-      prop[0,q]^-2
-    },
+    "Numerator" -> <|
+      "w1" -> 1/9 ee^2 dim Nc phat^-4 (d-2)^-1 (dot[p,p]+dot[p,q]) (-2 phat^2-dot[p,p]+d dot[p,p]+dim phat^2),
+      "w0" -> 1/9 ee^2 dim Nc phat^-2 (d-2)^-1 (dot[p,p]+dot[p,q]) (-2 phat^2+dot[p,p]+dim phat^2),
+      "w2" -> 1/9 ee^2 dim Nc (2 dot[p,q]^2-dot[p,p] dot[q,q]+dot[p,q] dot[q,q])
+    |>,
     "embedding" -> {
       1,
       0,
       -1
     },
-    "Numerator" -> <|
-      "w2" -> 4/9 ee^2 Nc (2 dot[p,q]^2-dot[p,p] dot[q,q]+dot[p,q] dot[q,q]),
-      "w1" -> 4/9 ee^2 Nc phat^-4 (d-2)^-1 (dot[p,p]+dot[p,q]) (2 phat^2-dot[p,p]+d dot[p,p]),
-      "w0" -> 4/9 ee^2 Nc phat^-2 (d-2)^-1 (2 phat^2+dot[p,p]) (dot[p,p]+dot[p,q])
-    |>
+    "Denominator" -> prop[0,p]^-1 prop[0,p+q]^-1,
+    "Partial_fraction" -> {
+      prop[0,p]^-1 prop[0,p+q]^-1
+    }
   |>,
   <|
-    "Denominator" -> prop[0,q]^-2,
+    "Denominator" -> prop[0,-p]^-1 prop[0,-p-q]^-1,
     "Numerator" -> <|
-      "w2" -> 4/9 ee^2 Nc (2 dot[p,q]^2-dot[p,p] dot[q,q]+dot[p,q] dot[q,q]),
-      "w0" -> 4/9 ee^2 Nc phat^-2 (d-2)^-1 (2 phat^2+dot[p,p]) (dot[p,p]+dot[p,q]),
-      "w1" -> 4/9 ee^2 Nc phat^-4 (d-2)^-1 (dot[p,p]+dot[p,q]) (2 phat^2-dot[p,p]+d dot[p,p])
+      "w0" -> 1/9 ee^2 dim Nc phat^-2 (d-2)^-1 (dot[p,p]+dot[p,q]) (-2 phat^2+dot[p,p]+dim phat^2),
+      "w1" -> 1/9 ee^2 dim Nc phat^-4 (d-2)^-1 (dot[p,p]+dot[p,q]) (-2 phat^2-dot[p,p]+d dot[p,p]+dim phat^2),
+      "w2" -> 1/9 ee^2 dim Nc (2 dot[p,q]^2-dot[p,p] dot[q,q]+dot[p,q] dot[q,q])
     |>,
-    "Partial_fraction" -> {
-      prop[0,q]^-2
-    },
     "embedding" -> {
       1,
       1,
       1
+    },
+    "Partial_fraction" -> {
+      prop[0,-p]^-1 prop[0,-p-q]^-1
     }
   |>
 }
